6365c5fe5d64fa4cb43c88455a1505508186b261
Lesia Karlova <lesia.karlova@nixsolutions.com>
2023-01-05 21:23:37 +0200

    Fix axios instance

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import serveStatic from 'serve-static';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 8080;
+
+app.listen(port);
+
+console.log('server started ' + port);
--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
-// import * as http from "http";
 
 const instance = axios.create({
   // baseURL: import.meta.env.VITE_APP_API_URL,
-  baseURL: "https://follow-ukraine-vue.herokuapp.com",
+  baseURL: "https://json-server.herokuapp.com",
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
